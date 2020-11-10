161332519f954a9afc045ef06ae70b613ee5e2b4
zackosowski <ZACHARYO@MYSTMA.ORG>
2020-11-10 20:44:43 +0000

    Created Assignment

--- JavaTemplate/src/app/App.java
+++ JavaTemplate/src/app/App.java
@@ -1,3 +1,16 @@
+/*
+METHODS 2
+
+Create a program that prints out all elements of a string array.
+Create a method that does this task call printArray
+
+LIMITATIONS:
+Maximmum one sysout
+Maximum one method
+
+*/
+
+
 package app;
 
 public class App {
